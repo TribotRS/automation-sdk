92a547b123992ff02761844e7baa41fa50724f1f
Nullable <Nullable@tribot.org>
2026-04-02 21:17:23 -0400

    more api

--- src/main/kotlin/org/tribot/automation/script/core/Camera.kt
+++ src/main/kotlin/org/tribot/automation/script/core/Camera.kt
@@ -3,12 +3,17 @@ package org.tribot.automation.script.core
 import net.runelite.api.Actor
 import net.runelite.api.coords.WorldPoint
 
+enum class CameraMethod {
+    KEYS,
+    MOUSE
+}
+
 interface Camera {
     fun getAngle(): Int
     fun getRotation(): Int
     fun getZoom(): Double
-    fun setAngle(angle: Int)
-    fun setRotation(rotation: Int)
-    fun turnTo(point: WorldPoint)
-    fun turnTo(actor: Actor)
+    fun setAngle(angle: Int, method: CameraMethod)
+    fun setRotation(rotation: Int, method: CameraMethod)
+    fun turnTo(point: WorldPoint, method: CameraMethod)
+    fun turnTo(actor: Actor, method: CameraMethod)
 }
