ffa4ac50aff70489b128e2fb601c59178cfe110e
Nullable <Nullable@tribot.org>
2026-04-10 23:41:12 -0400

    Add better client thread func

--- src/main/kotlin/org/tribot/automation/script/core/ClientThread.kt
+++ src/main/kotlin/org/tribot/automation/script/core/ClientThread.kt
@@ -28,6 +28,11 @@ interface ClientThread {
     fun executeBlocking(block: Runnable)
 
     /**
+     * See [executeBlocking].
+     */
+    fun <T>executeBlocking(block: () -> T): T?
+
+    /**
      * Gets Runelite's client thread instance that can invoke methods on the client thread using their technique. This
      * is slower than [executeBlocking].
      */
