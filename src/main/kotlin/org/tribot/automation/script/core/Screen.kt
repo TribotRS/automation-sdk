a07bed8fba353ee3dc4524a9f6218073af167d79
Nullable <Nullable@tribot.org>
2026-04-14 18:20:02 -0400

    API fixes

--- src/main/kotlin/org/tribot/automation/script/core/Screen.kt
+++ src/main/kotlin/org/tribot/automation/script/core/Screen.kt
@@ -4,7 +4,17 @@ import java.awt.Point
 import java.awt.Rectangle
 
 interface Screen {
-    fun getViewportBounds(): Rectangle
+    /**
+     * Gets the canvas dimension, including HUD and minimap.
+     */
+    fun getCanvasDimensions(): Rectangle
 
+    /**
+     * Checks if a given point is within the viewport, calculated as any area of the screen that isn't covered by the
+     * "fixed" HUD widgets (chat, tabs, minimap, etc.). Works in both fixed and resizable modes.
+     *
+     * This method uses pre-calculated HUD dimensions for performance, which means it does NOT work with any runelite plugins
+     * that change the HUD dimensions/locations.
+     */
     fun isPointInViewport(point: Point): Boolean
 }
