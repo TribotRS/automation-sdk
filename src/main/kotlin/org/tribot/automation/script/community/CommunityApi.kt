ef72735c6d0faf58aa2e88ed582b3c4bcd1815c9
WreckedScripts <stefano@onlinepaymentplatform.com>
2026-06-09 15:17:24 +0200

    [Antiban/Randomization] Introduce Lottery helper to facilitate randomization within scripts, based on probability with allowance of a guaranteed condition / execution

--- src/main/kotlin/org/tribot/automation/script/community/CommunityApi.kt
+++ src/main/kotlin/org/tribot/automation/script/community/CommunityApi.kt
@@ -1,8 +1,14 @@
 package org.tribot.automation.script.community
 
 import org.tribot.automation.script.ScriptContext
+import org.tribot.automation.script.community.antiban.randomization.Lottery
 import org.tribot.automation.script.community.widgets.MakeScreen
 
 class CommunityApi(ctx: ScriptContext) {
     val makeScreen = MakeScreen(ctx)
+
+    /**
+     * Antiban -> Randomization
+     */
+    val lottery = Lottery(ctx)
 }
